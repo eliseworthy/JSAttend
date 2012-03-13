9218c835930a4ab5d4bdf3588e99dd181037927a
Elise Worthy <elise.worthy@livingsocial.com>
2012-03-12 21:47:23 -0400

    added commands

--- jstwitter.rb
+++ jstwitter.rb
@@ -9,12 +9,38 @@ class JSTwitter
     @client = JumpstartAuth.twitter
   end
 
+  def run
+    puts "Welcome to the JSTwitter client!"
+
+    command = ""
+    while command != "q"
+      printf "enter command: "
+      
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      message = parts[1..-1].join(" ")
+
+      case command
+      when 'q' then puts 'See ya later!'
+      when 't' then tweet(message)
+      else
+        puts "What the heck are you talking about with #{command}?"
+      end
+    end
+  end
+
   def tweet(message)
-    @client.update(message)
+
+    if message.length >= 140
+      puts "your tweet is too dang long, son."
+    else
+      @client.update(message)
+    end
   end
 
 end
 
 #Script
 jst = JSTwitter.new
-jst.tweet("JSTwitter initialized by @eliseworthy")
+jst.run
