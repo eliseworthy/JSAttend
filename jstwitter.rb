65326db68b543639d8fd31bac1e0eee7e689ae5b
Elise Worthy <elise.worthy@livingsocial.com>
2012-03-12 22:44:35 -0400

    added spam and everyone's last tweet (couldn't test because of Twitter errors)

--- jstwitter.rb
+++ jstwitter.rb
@@ -25,6 +25,8 @@ class JSTwitter
       when 'q' then puts 'See ya later!'
       when 't' then tweet(message)
       when 'dm' then dm(parts[1], parts[2..-1].join(" "))  
+      when 's' then spam_my_followers(message) 
+      when 'elt' then everyones_last_tweet 
       else
         puts "What the heck are you talking about with #{command}?"
       end
@@ -48,6 +50,34 @@ class JSTwitter
       else
         puts "You ain't got no love from #{target}"
       end
+  end
+
+  def followers_list
+    screen_names = @client.followers.collect{|follower| follower.screen_name}
+    #@client.followers.collect(&:screen_name)
+    return screen_names
+  end
+
+  def spam_my_followers(message)
+    followers_list.each do |target|
+      dm(target, message)
+    end
+    puts "Spam complete!"
+  end
+
+  def everyones_last_tweet
+    friends = @client.friends
+    friends = friends.sort_by{|friend| screen_name.downcase}
+
+    friends.each do |friend|
+
+      timestamp = friend.status.created_at
+      tweet_date = Date.parse(timestamp)
+
+      puts "#{friend.screen_name} said the following at #{tweet_date.strftime("%A, %b %d")}:"
+      puts friends.status.text
+      puts "" 
+    end
   end
 
 end
